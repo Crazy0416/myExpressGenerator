bab3611b13a4bfcb21e7514a0b4b9d7ed56328d5
kimminho <minmin0416@naver.com>
2018-08-23 14:49:38 +0900

    Config: Delete morgan logger.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const errorHandle = require('./errors/errorHandle');
 
 const indexRouter = require('./routes/index');
@@ -20,7 +19,6 @@ const mongoHandler = require('./helpers/mongooseHandler');
 mongoHandler.connect();
 
 // app use setup
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
