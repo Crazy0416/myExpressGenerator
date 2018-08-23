b3d2f1082c7449fceca8cb156fea561eb15b0fac
kimminho <minmin0416@naver.com>
2018-08-23 15:47:38 +0900

    Config: Add README file and pm2 config file.

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,8 +4,7 @@ const wrap = require('express-async-wrap');
 
 const UserCtrl = require('../controllers/UserCtrl');
 
-// 회원가입
 router.route('/users/register')
-    .post(wrap(UserCtrl.register));
+	.post(wrap(UserCtrl.register)); // 회원가입
 
 module.exports = router;
